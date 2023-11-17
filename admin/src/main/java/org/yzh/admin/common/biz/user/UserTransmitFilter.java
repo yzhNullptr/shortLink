d2bd1bad923ffdfea862372368efc40602f1e18c
yzh_nullptr <yzh_nullptr@outlook.com>
2023-11-17 11:27:54 +0800

    feature：开发用户拦截器中忽略登录接口

--- admin/src/main/java/org/yzh/admin/common/biz/user/UserTransmitFilter.java
+++ admin/src/main/java/org/yzh/admin/common/biz/user/UserTransmitFilter.java
@@ -2,14 +2,17 @@ package org.yzh.admin.common.biz.user;
 
 
 import com.alibaba.fastjson2.JSON;
+import com.google.common.collect.Lists;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.yzh.admin.common.constant.RedisCacheConstant;
 import org.yzh.admin.common.constant.UserConstant;
 
 import java.io.IOException;
+import java.util.List;
 
 /**
  * 用户信息传输过滤器
@@ -18,19 +21,18 @@ import java.io.IOException;
 public class UserTransmitFilter implements Filter {
 
     private final StringRedisTemplate stringRedisTemplate;
+    private static final List<String> IGNORE_URI= Lists.newArrayList(
+            "/api/shortLink/v1/user/login",
+            "/api/shortLink/v1/user/has-username"
+    );
     @Override
+    @SneakyThrows
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-//        String userId = httpServletRequest.getHeader(UserConstant.USER_ID_KEY);
-//        if (StringUtils.hasText(userId)) {
+        String requestURI = httpServletRequest.getRequestURI();
+        if (!IGNORE_URI.contains(requestURI)){
+            String method=httpServletRequest.getMethod();
             String userName = httpServletRequest.getHeader(UserConstant.USER_NAME_KEY);
-//            String realName = httpServletRequest.getHeader(UserConstant.REAL_NAME_KEY);
-//            if (StringUtils.hasText(userName)) {
-//                userName = URLDecoder.decode(userName, UTF_8);
-//            }
-//            if (StringUtils.hasText(realName)) {
-//                realName = URLDecoder.decode(realName, UTF_8);
-//            }
             String token = httpServletRequest.getHeader(UserConstant.USER_TOKEN_KEY);
             String key= RedisCacheConstant.USER_LOGIN_KEY+userName;
             Object userInfoJsonStr = stringRedisTemplate.opsForHash().get(key, token);
@@ -38,7 +40,8 @@ public class UserTransmitFilter implements Filter {
                 UserInfoDTO userInfoDTO = JSON.parseObject(userInfoJsonStr.toString(), UserInfoDTO.class);
                 UserContext.setUser(userInfoDTO);
             }
-//        }
+        }
+
         try {
             filterChain.doFilter(servletRequest, servletResponse);
         } finally {
