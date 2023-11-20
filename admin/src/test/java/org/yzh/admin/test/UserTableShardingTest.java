ef90c9409a352476d0f35e4a2167953998e8e844
yzh_nullptr <yzh_nullptr@outlook.com>
2023-11-20 21:48:20 +0800

    feature:短链接分组分表处理

--- admin/src/test/java/org/yzh/admin/test/UserTableShardingTest.java
+++ admin/src/test/java/org/yzh/admin/test/UserTableShardingTest.java
@@ -1,29 +1,21 @@
 package org.yzh.admin.test;
 
 public class UserTableShardingTest {
-    public static final String SQL= "create table t_link_%d\n" +
+    public static final String SQL= "create table t_group_%d\n" +
             "(\n" +
-            "    id              bigint auto_increment comment 'ID'\n" +
+            "    id          bigint auto_increment comment 'ID'\n" +
             "        primary key,\n" +
-            "    domain          varchar(128) charset utf8mb4  null comment '域名',\n" +
-            "    short_uri       varchar(8) charset utf8mb4    null comment '短链接',\n" +
-            "    full_short_url  varchar(128) charset utf8mb4  null comment '完整短链接',\n" +
-            "    origin_url      varchar(1024) charset utf8mb4 null comment '原始链接',\n" +
-            "    click_num       int                           null comment '点击量',\n" +
-            "    gid             varchar(32) charset utf8mb4   null comment '分组标识',\n" +
-            "    favicon         varchar(256)                  null comment '网站图标',\n" +
-            "    enable_status   tinyint                       null comment '启用标识 0：启用 1：未启用',\n" +
-            "    create_type     tinyint                       null comment '创建类型 0：接口创建 1：控制台创建',\n" +
-            "    valid_date_type tinyint                       null comment '有效期类型 0：永久有效 1：自定义有效期',\n" +
-            "    valid_date      datetime                      null comment '有效期',\n" +
-            "    `describe`      varchar(1024) charset utf8mb4 null comment '描述',\n" +
-            "    create_time     datetime                      null comment '创建时间',\n" +
-            "    update_time     datetime                      null comment '更新时间',\n" +
-            "    del_flag        tinyint                       null comment '删除标识',\n" +
-            "    constraint idx_unique_full_short_url\n" +
-            "        unique (full_short_url)\n" +
+            "    gid         varchar(32)  null comment '分组标识',\n" +
+            "    name        varchar(64)  null comment '分组名称',\n" +
+            "    username    varchar(256) null comment '创建分组用户名',\n" +
+            "    sort_order  int          null comment '分组排序',\n" +
+            "    create_time datetime     null comment '创建时间',\n" +
+            "    update_time datetime     null comment '修改时间',\n" +
+            "    del_flag    tinyint(1)   null comment '删除标识 0：未删除 1：已删除',\n" +
+            "    constraint idx_unique_username_gid\n" +
+            "        unique (gid, username)\n" +
             ")\n" +
-            "    collate = utf8mb3_bin;";
+            "    charset = utf8mb4;";
     public static void main(String[] args) {
         for (int i = 0; i < 16; i++) {
             System.out.printf((SQL) + "%n",i);
