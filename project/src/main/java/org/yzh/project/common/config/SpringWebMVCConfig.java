a75bd1aa6fd70f4d512385db82077ddc943d48aa
yzh_nullptr <yzh_nullptr@outlook.com>
2023-11-22 18:07:58 +0800

    refactor:更改为配置文件 配置静态资源映射

--- project/src/main/java/org/yzh/project/common/config/SpringWebMVCConfig.java
+++ project/src/main/java/org/yzh/project/common/config/SpringWebMVCConfig.java
@@ -1,10 +1,9 @@
 package org.yzh.project.common.config;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+//@Configuration
 public class SpringWebMVCConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
